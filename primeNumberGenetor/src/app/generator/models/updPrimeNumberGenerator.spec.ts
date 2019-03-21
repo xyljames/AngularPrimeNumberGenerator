cb19bbc695f493069ca91183e9565347e05720c4
Yilang Xuan <xyljames@gmail.com>
2019-03-20 22:21:18 -0400

    generate test cases => all pass

--- primeNumberGenetor/src/app/generator/models/updPrimeNumberGenerator.spec.ts
+++ primeNumberGenetor/src/app/generator/models/updPrimeNumberGenerator.spec.ts
@@ -1,5 +1,6 @@
 import { UpdPrimeNumGenerator } from "./uddPrimeNumberGenerator";
 import { ComponentFixture, async, TestBed } from "@angular/core/testing";
+import { start } from "repl";
 
 describe('UpdPrimeNumberGenerator class test cases',()=>{
     let component:UpdPrimeNumGenerator;
@@ -17,5 +18,31 @@ describe('UpdPrimeNumberGenerator class test cases',()=>{
     });
     it('should create',()=>{
         expect(component).toBeTruthy()
+    })
+    describe('generate test cases',()=>{
+        it('given a range, isPrime should be called',()=>{
+            let startNum = 1;
+            let endNum = 5;
+            let spyPrime = spyOn(component,'isPrime').and.returnValue(true);
+            component.generate(startNum,endNum);
+            expect(spyPrime).toHaveBeenCalled();
+            expect(component.generate(startNum,endNum)).not.toBeNull()
+            
+        })
+
+        it('if start or end number is invalid',()=>{
+            let startNum = 1;
+            expect(component.generate(startNum,null)).toBeTruthy()
+            let spyPrimeFalse = spyOn(component,'isPrime').and.returnValue(false);
+            expect(spyPrimeFalse).not.toHaveBeenCalled();
+        })
+
+        it('if isPrime(value) is true, push to list',()=>{
+            let startNum =1;
+            let endNum = 5;
+            component.generate(startNum,endNum);
+            component.isPrime(startNum);
+            expect(component.generate(startNum,endNum)).not.toBeNull()    
+        })
     })
 })
