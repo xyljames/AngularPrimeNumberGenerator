a3a48313ee1c062eca9722149bd057b8cd1726a3
Yilang Xuan <xyljames@gmail.com>
2019-03-20 22:11:57 -0400

    UpdPrimeNumberGenerator component class should be create

--- primeNumberGenetor/src/app/generator/models/updPrimeNumberGenerator.spec.ts
+++ primeNumberGenetor/src/app/generator/models/updPrimeNumberGenerator.spec.ts
@@ -1,5 +1,21 @@
 import { UpdPrimeNumGenerator } from "./uddPrimeNumberGenerator";
+import { ComponentFixture, async, TestBed } from "@angular/core/testing";
 
 describe('UpdPrimeNumberGenerator class test cases',()=>{
     let component:UpdPrimeNumGenerator;
+    let fixture:ComponentFixture<UpdPrimeNumGenerator>;
+
+    beforeEach(async(()=>{
+        TestBed.configureTestingModule({
+            declarations:[UpdPrimeNumGenerator],
+        }).compileComponents();
+    }));
+    beforeEach(()=>{
+        fixture=TestBed.createComponent(UpdPrimeNumGenerator);
+        component = fixture.componentInstance;
+        fixture.detectChanges()
+    });
+    it('should create',()=>{
+        expect(component).toBeTruthy()
+    })
 })
